b8f65dac8b3b9338d43e0d9941c836fa10102107
bilal asghar <asgharbilal8@gmail.com>
2025-12-03 16:03:11 +0000

    add tailwind css and fontawesome

---
+++ frontend/src/Utilities/fontawesome.js
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'; // all solid icons
+import { fab } from '@fortawesome/free-brands-svg-icons'; // all brands
+import { far } from '@fortawesome/free-regular-svg-icons'; // all regular
+
+library.add(fas, fab, far);
