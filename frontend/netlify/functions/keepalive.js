5240096007e2ca332d9c2f0916ea5fb1fc518b65
bilal asghar <asgharbilal8@gmail.com>
2025-12-08 13:59:12 +0000

    add netlify background job to keep render web service running without spinning down

---
+++ frontend/netlify/functions/keepalive.js
@@ -0,0 +1,12 @@
+export default async () => {
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    try {
+        const response = await fetch(`${API_URL}/backend-keep-alive`);
+        const data = await response.json();
+
+        return { statusCode: 200, body: JSON.stringify(data) };
+    } catch (err) {
+        return { statusCode: 500, body: "Failed to ping Render" };
+    }
+};
